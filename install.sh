003b2afd126fbbf9f2315c7021d25dc2cc454d09
Thea Barnes <thea@tsbarnes.com>
2020-11-23 10:06:44 -0500

    Added install script

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+DRY_RUN="false"
+
+for option in "$@"; do
+  case $option in
+  "--dry-run")
+    DRY_RUN="true"
+    ;;
+  *)
+    echo "Unknown option: $option"
+    exit 1
+    ;;
+  esac
+done
+
+if [ "$USER" = "root" ]; then
+  INSTALL_PATH="$HOME/.local"
+else
+  INSTALL_PATH="/usr/local"
+fi
+
+if [ "$DRY_RUN" = "false" ]; then
+  cp Daniela.colors "$INSTALL_PATH/color-schemes/"
+  cp -r com.tsbarnes.Daniela "$INSTALL_PATH/plasma/look-and-feel/"
+  echo "Installation complete!"
+else
+  echo cp Daniela.colors "$INSTALL_PATH/color-schemes/"
+  echo cp -r com.tsbarnes.Daniela "$INSTALL_PATH/plasma/look-and-feel/"
+  echo "Dry run, nothing installed."
+fi
+
+exit 0
